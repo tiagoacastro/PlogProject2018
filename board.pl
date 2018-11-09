15fef4fb78355f3e66f0747814a4b10fa51d42bb
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-11-09 17:06:49 +0000

    board coords placed

--- board.pl
+++ board.pl
@@ -8,14 +8,18 @@ startBoard( [['x', 'w', 'x', 'w', 'x'],
              ['x', 'b', 'x', 'b', 'x']] ).
 
 %Displays the board
-display_game([], Player) :-
-    display_border(28).
+display_gameAux([], Player, _) :-
+    display_border(31),
+    display_columns.
 
-display_game([A|B], Player) :-
-    display_border(28),
-    put_code(0x2503),
-    display_line(A, Player),
-    display_game(B, Player).
+display_gameAux([A|B], Player, N) :-
+    display_border(31),
+    display_line(A, Player, N),
+    New is N+1,
+    display_gameAux(B, Player, New).
+
+display_game(List, Player) :-
+    display_gameAux(List, Player, 1).
 
 display_border(0) :- 
     write('\n').
@@ -26,15 +30,20 @@ display_border(N) :-
     N1 is N-1,
     display_border(N1).
 
-display_line([], Player) :-
+display_lineAux([], Player) :-
     write('\n').
 
-display_line([A|B], Player) :-
+display_lineAux([A|B], Player) :-
     write(' '),
     write_char(A),
     write(' '),
     put_code(0x2503),
-    display_line(B, Player).
+    display_lineAux(B, Player).
+
+display_line(List, Player, N) :-
+    format('~w ', N),
+    put_code(0x2503),
+    display_lineAux(List,Player).
 
 write_char('x') :-
     write(' ').
@@ -44,3 +53,18 @@ write_char('w') :-
 
 write_char('b') :-
     put_code(0x25CF).
+
+display_columns :- 
+    write('  '),
+    put_code(0x2503),
+    write(' a '),
+    put_code(0x2503),
+    write(' b '),
+    put_code(0x2503),
+    write(' c '),
+    put_code(0x2503),
+    write(' d '),
+    put_code(0x2503),
+    write(' e '),
+    put_code(0x2503),
+    write('\n').

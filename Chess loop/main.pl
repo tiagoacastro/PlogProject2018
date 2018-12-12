e302a35c4c3348d192d76e0cddbca55f6cb4eb70
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-12-12 18:33:56 +0000

    2 problems need solving: ciclos repetidos e peças obstruidas movem a mm

--- Chess loop/main.pl
+++ Chess loop/main.pl
@@ -1,46 +1,6 @@
 :- use_module(library(clpfd)).
 :- include('utilities.pl').
 
-/*
-nqueens(N,D,Cols):-
-    length(Cols,N),
-    domain(Cols,1,D),
-    constrain(Cols),
-    all_distinct(Cols), % Redundante mas diminui o tempo de resolução
-    labeling([ff],Cols).
-
-constrain([]).
-
-constrain([H | RCols]):-
-    safe(H,RCols,1),
-    constrain(RCols).
-
-safe(_,[],_).
-
-safe(X,[Y | T], K) :-
-    noattack(X,Y,K),
-    K1 is K + 1,
-    safe(X,T,K1).
-
-noattack(X,Y,K) :-
-    X #\= Y,
-    X + K #\= Y,
-    X - K #\= Y.
-   
-nrooks(N, Cols) :-
-    length(Cols,N),
-    domain(Cols,1,N),
-    all_distinct(Cols),
-    labeling([ff], Cols).
-
-nbishop(N, Rows, Cols) :-
-    length(Cols, N),
-    length(Rows, N),
-    domain(Cols, 1, N),
-    domain(Rows, 1, N).
-    constBishop(Cols, Rows).
-*/
-
 %Solves the position of the pieces
 solve(Npieces, Nrows, Ncols, Type1, Type2, Types, Rows, Cols) :-
     length(Types, Npieces),

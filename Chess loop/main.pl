feaff5e3e2301ae5017da5551c0603e91d801b72
Tiago Castro <tiagoaraujocastro@gmail.com>
2018-12-12 11:07:47 +0000

    Project layout done, only missing restricitons of moves

--- Chess loop/main.pl
+++ Chess loop/main.pl
@@ -1,10 +1,11 @@
 :- use_module(library(clpfd)).
 
+/*
 nqueens(N,D,Cols):-
     length(Cols,N),
     domain(Cols,1,D),
     constrain(Cols),
-    % all_distinct(Cols), % Redundante mas diminui o tempo de resolução
+    all_distinct(Cols), % Redundante mas diminui o tempo de resolução
     labeling([ff],Cols).
 
 constrain([]).
@@ -31,16 +32,65 @@ nrooks(N, Cols) :-
     all_distinct(Cols),
     labeling([ff], Cols).
 
-
 nbishop(N, Rows, Cols) :-
     length(Cols, N),
     length(Rows, N),
     domain(Cols, 1, N),
     domain(Rows, 1, N).
     constBishop(Cols, Rows).
+*/
 
-%constBishop([HRow|TRow], [HCol|TCol]) :-
+%Solves the position of the pieces
+solve(Npieces, Nrows, Ncols, Type1, Type2, Types, Rows, Cols) :-
+    length(Types, Npieces),
+    length(Rows, Npieces),
+    domain(Rows, 1, Nrows),
+    length(Cols, Npieces),
+    domain(Cols, 1, Ncols),
+    setTypes(Types, Type1, Type2, Npieces),
+    setup(Types, Rows, Cols),
+    labeling([ff], Rows),
+    labeling([ff], Cols).
+
+%Sets up the iteration function
+setup(Types, [R1|Rr], [C1|Cr]):-
+    iterate(Types, [R1|Rr], [C1|Cr], R1, C1).
     
+%Base case for the iteration of the list, last pieces eats the first one
+iterate([H|[]], [R1|[]], [C1|[]], Fr, Fc):-
+    eat(H, R1, Fr, C1, Fc).
+    
+%General case for the iteration of the list, a piece eats the next one
+iterate([H|Tr], [R1,R2|Rr], [C1,C2|Cr], Fr, Fc):-
+    eat(H, R1, R2, C1, C2),
+    iterate(Tr, [R2|Rr], [C2|Cr], Fr, Fc).
+    
+%King move
+eat(1, R1, R2, C1, C2).
 
+%Queen move
+eat(2, R1, R2, C1, C2).
 
+%Rook move
+eat(3, R1, R2, C1, C2). 
+    
+%Bishop move
+eat(4, R1, R2, C1, C2). 
 
+%Knight move
+eat(5, R1, R2, C1, C2). 
+
+setTypes([], _, _, 0).
+
+%Alternates the types of the ordered pieces, so that after the move of a type1 piece, a type2 move follows and vice-versa.
+setTypes([H|R], Type1, Type2, Npieces):-
+    Type is Npieces mod 2,
+    Next is Npieces - 1,
+    giveType(H, Type1, Type2, Type),
+    setTypes(R, Type1, Type2, Next).
+
+%Sets the type1 for the even pieces
+giveType(Type1, Type1, Type2, 0).
+
+%Sets the type2 for the odd pieces
+giveType(Type2, Type1, Type2, 1).
